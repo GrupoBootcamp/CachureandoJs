c61d3e36d5d60e7b7a4b2ab831fede6f51d136d1
Miguel Rondanelli <miguel.rondanelli.inf@gmail.com>
2023-01-31 13:24:46 -0300

    commit rama Miguel agregada funcionalidad encuensta

--- js/main.js
+++ js/main.js
@@ -1,3 +1,62 @@
+const select = document.querySelector('#Selector');
+const resultado = document.querySelector('#resultado');
+let encuesta = document.querySelector('#encuesta');
+
+console.log(encuesta.textContent);
+
+console.log(select);
+console.log('hola mundo');
+
+
+eventos();
+
+function eventos(){
+
+  select.addEventListener('click', mostrarSeleccionado);
+
+}
+
+
+
+function mostrarSeleccionado (e){
+
+    let x = e.target.value;
+    console.log(x);
+
+
+    switch(true){
+
+        case (x<=3):mostrarHtml('Muy deficiente');
+                    break;
+        case (x>3 && x<=5): mostrarHtml('insuficiente');
+                     break;
+        case (x>5 && x<=6): mostrarHtml('Suficiente');
+                    break;
+        case (x>6 && x<=7): mostrarHtml('Bien');
+                    break;
+        case (x>7 && x<=9): mostrarHtml('notable');
+                    break;
+        case (x>9): mostrarHtml('Sobresaliente');
+                    break;
+        default: mostrarHtml('Seleccione una opcion');
+                break
+
+    }
+
+}
+
+
+function mostrarHtml(valor){
+
+
+    resultado.textContent = valor;
+
+    
+}
+
+
+
+
 function startTime(){
     
     today=new Date();
